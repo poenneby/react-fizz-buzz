e0c389f9192aac6188e54afba186c2f2f16e22e3
Peter Onneby <ponneby@xebia.fr>
2022-04-07 23:17:39 +0200

    Starting point

---
+++ src/fizz-buzz/FizzBuzz.spec.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("Fizz Buzz", () => {
+  it("should pass", () => {
+    expect(false).toBeTruthy();
+  });
+});
