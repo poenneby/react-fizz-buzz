8dbb68146b7ea590dbd830e3e87cece0ee994bef
Peter Onneby <ponneby@xebia.fr>
2022-09-30 14:24:11 +0200

    Remove unused imports

--- src/fizz-buzz/FizzBuzz.spec.tsx
+++ src/fizz-buzz/FizzBuzz.spec.tsx
@@ -1,6 +1,5 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 describe("Fizz Buzz", () => {
   it("should pass", () => {
